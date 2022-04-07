7e80ade0000333fd839f2dcdf53d7650cff4bca5
Amir Raissi <55670960+amir-raissi@users.noreply.github.com>
2022-04-07 11:36:06 -0600

    Added dapp provider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,22 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { DAppProvider, Config, Goerli } from '@usedapp/core';
 import './styles/index.css';
 import App from './App';
 
+const config: Config = {
+	readOnlyChainId: Goerli.chainId,
+	readOnlyUrls: {
+		[Goerli.chainId]:
+			'https://eth-goerli.alchemyapi.io/v2/AxnmGEYn7VDkC4KqfNSFbSW9pHFR7PDO',
+	},
+};
+
 ReactDOM.render(
 	<StrictMode>
-		<App />
+		<DAppProvider config={config}>
+			<App />
+		</DAppProvider>
 	</StrictMode>,
 	document.getElementById('root')
 );
