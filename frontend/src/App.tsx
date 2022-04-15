1998fe9d712dc52a5074e2a49b4bbe5ac1d391c0
Matthew Hughes <42755431+zeepk@users.noreply.github.com>
2022-04-14 23:57:51 -0700

    added wallet connection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,23 @@ import SlotContainer from './components/SlotContainer';
 import CustomNavbar from './components/CustomNavbar';
 import './styles/App.css';
 
+import { Mainnet, DAppProvider, Config } from '@usedapp/core';
+import { getDefaultProvider } from 'ethers';
+
+const config: Config = {
+	readOnlyChainId: Mainnet.chainId,
+	readOnlyUrls: {
+		[Mainnet.chainId]: getDefaultProvider('mainnet'),
+	},
+};
+
 function App() {
 	return (
-		<div>
-			<CustomNavbar />
-			<SlotContainer />
+		<div className="app">
+			<DAppProvider config={config}>
+				<CustomNavbar />
+				<SlotContainer />
+			</DAppProvider>
 		</div>
 	);
 }
--- frontend/src/components/SlotContainer.tsx
+++ frontend/src/components/SlotContainer.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import SlotMachine from 'react-slot-machine-gen';
 import Slots from '../assets/slots.jpg';
 import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { useEtherBalance, useEthers } from '@usedapp/core';
+import { formatEther } from '@ethersproject/units';
 
 function SlotContainer() {
+	const { activateBrowserWallet, account, deactivate } = useEthers();
+	const etherBalance = useEtherBalance(account);
 	const [play, setPlay] = React.useState(false);
 	const [spinning, setSpinning] = React.useState(false);
 	const icons: string[] = [
@@ -20,7 +28,7 @@ function SlotContainer() {
 	];
 
 	// hardcoded height of slot image
-	const height = 1181;
+	const height = 1241;
 	const total = icons.length;
 	const individualHeight = height / total;
 	const centerOffset = individualHeight / 2;
@@ -46,27 +54,67 @@ function SlotContainer() {
 			symbols,
 		},
 	];
+
 	const handleResult = (results) => {
 		setSpinning(false);
 		console.log(results.map((r) => r.title));
 	};
-	return (
-		<div className="container--slots">
+
+	const handlePlay = () => {
+		setSpinning(true);
+		setPlay(!play);
+	};
+
+	const actionButton = account ? (
+		<div className="container--actions">
 			<Button
 				className="button--slots"
 				variant="contained"
 				disabled={spinning}
-				onClick={() => {
-					setSpinning(true);
-					setPlay(!play);
-				}}
+				onClick={() => handlePlay()}
 			>
 				{spinning ? 'Good luck!' : 'Play'}
 			</Button>
+			<Button
+				className="button--deactivate"
+				variant="contained"
+				color="error"
+				onClick={() => deactivate()}
+			>
+				Disconnect
+			</Button>
+		</div>
+	) : (
+		<div className="container--connect">
+			<Button
+				onClick={() => activateBrowserWallet()}
+				variant="contained"
+				color="info"
+			>
+				Connect Wallet
+			</Button>
+		</div>
+	);
+	return (
+		<div className="container--slots">
+			<Card className="card" sx={{ minWidth: 275 }}>
+				<CardContent>
+					<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+						{account ? 'Account balance' : 'Welcome!'}
+					</Typography>
+					<Typography sx={{ mt: 1.5 }} variant="h3" component="div">
+						{!!etherBalance && formatEther(etherBalance)}
+					</Typography>
+					<Typography sx={{ mb: 1.5 }} color="text.secondary">
+						{account && 'eth'}
+					</Typography>
+				</CardContent>
+				<CardActions>{actionButton}</CardActions>
+			</Card>
 			<SlotMachine
 				reels={reels}
 				play={play}
-				options={{ reelHeight: height, reelWidth: 100 }}
+				options={{ reelHeight: height, reelWidth: 100, reelOffset: 20 }}
 				callback={(e) => handleResult(e)}
 			/>
 		</div>
